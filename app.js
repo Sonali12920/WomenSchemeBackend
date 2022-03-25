e8b0475e3c66b26bd63f83070e74af77109aa00a
sonali <sona120920@gmail.com>
2022-03-25 22:45:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app  = express();
+
+const db = 'mongodb+srv://minorproj:minor2022@cluster0.lywnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(db).then(() => {
+    console.log("Database connected")
+}).catch((err) => {
+    console.log("not connected")
+})
+
+
+const middleware = (req, res, next) => {
+    console.log("Middleware");
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.get('/scholarships', middleware, (req, res) => {
+    res.send("Helloooo")
+})
+
+app.listen(3000, () => {
+    console.log('server is running')
+})
