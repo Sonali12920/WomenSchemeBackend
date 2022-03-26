894061d3a9863be9f2c3c1415c5dda69cfc19b96
nivedita0604 <niveditakumari0604@gmail.com>
2022-03-26 19:17:03 +0530

    start script added

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express from "express";
+import mongoose from "mongoose";
+//const express = require('express');
+//const mongoose = require("mongoose");
 
-const app  = express();
+const app = express();
 
-const db = 'mongodb+srv://minorproj:minor2022@cluster0.lywnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const db =
+  "mongodb+srv://minorproj:minor2022@cluster0.lywnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(db).then(() => {
-    console.log("Database connected")
-}).catch((err) => {
-    console.log("not connected")
-})
-
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("not connected");
+  });
 
 const middleware = (req, res, next) => {
-    console.log("Middleware");
-    next();
-}
+  console.log("Middleware");
+  next();
+};
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.get('/scholarships', middleware, (req, res) => {
-    res.send("Helloooo")
-})
+app.get("/scholarships", middleware, (req, res) => {
+  res.send("Helloooo");
+});
 
 app.listen(3000, () => {
-    console.log('server is running')
-})
+  console.log("server is running");
+});
